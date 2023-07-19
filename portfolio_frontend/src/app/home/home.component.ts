4fd9f9b8d8d4d1df1202bebdd472c92b2377ba5d
Ethan.Janicki <ethan.janicki@ucf.edu>
2023-07-19 16:01:21 -0400

    feat: add name with fade in-out

--- portfolio_frontend/src/app/home/home.component.ts
+++ portfolio_frontend/src/app/home/home.component.ts
@@ -1,10 +1,48 @@
 import { Component } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('fadeAnimation', [
+      state('visible', style({
+        opacity: 1
+      })),
+      state('hidden', style({
+        opacity: 0
+      })),
+      transition('* => hidden', [
+        animate('500ms ease-in-out'),
+      ]),
+      transition('* => visible', [
+        animate('500ms ease-in-out'),
+      ]),
+    ])
+  ]
 })
 export class HomeComponent {
+  constructor() { }
+
+  isHoveredOverName: boolean = false;
+  delayedIsHoveredOverName: boolean = false;
+
+  toggleHoverName() {
+    this.isHoveredOverName = !this.isHoveredOverName;
+  }
+  
+  setHoverName(state: boolean) {
+    this.isHoveredOverName = state;
+    this.setDelayedHoverName(state);
+  }
+
+  setDelayedHoverName(state: boolean) {
+    // wait 10 ms before setting the hover state
+    setTimeout(() => {
+      this.delayedIsHoveredOverName = state;
+    }
+    , 501);
+  }
 
 }
